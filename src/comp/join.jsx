21d44bc39f5343edc8d6d5fcc9cf1de892de2e06
pho-netik <oacidnim@g.ucla.edu>
2017-08-28 23:03:59 -0700

    add join banner, icons, responsiveness

--- src/comp/join.jsx
+++ src/comp/join.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+var banner = require('../assets/join.png')
+var discord = require('../assets/discord.svg')
+var facebook = require('../assets/facebook.svg')
+var mail = require('../assets/mail.png')
 
 const JoinBanner = () => {
         return (
             <div className="join mainelement">
-                {/* insert join banner svg (?) */}
+                <img className="banner" src={banner} alt='' />
                 <div className="join-container">
-                <div className="discord link">
+                <div className="icon"  className="discord link">
                     <h1>can't wait to hang out?</h1>
-                    {/* insert svg */}
+                    <img src={discord} alt=''/><br></br>
                     Join Our Discord!
                 </div>
                 <div className="facebook link">
                     <h1>Follow Us On</h1>
-                    {/* insert svg */}
+                    <img className="icon" src={facebook} alt=''/><br></br>
                     For the latest updates on club events!
                 </div>
                 <div className="mail link">
                     <h1>Join Our Mailing List</h1>
-                    {/* insert svg */}
+                    <img className="icon" src={mail} alt=''/><br></br>
                     For Weekly Announcements
                 </div>
                 </div>
