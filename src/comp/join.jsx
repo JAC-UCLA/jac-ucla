4c918d60bea4f7c7f34ecdcedd4cf0e85baa1467
pho-netik <oacidnim@g.ucla.edu>
2017-08-29 01:32:28 -0700

    edit about section css, navbar hover, and join width/links

--- src/comp/join.jsx
+++ src/comp/join.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 var banner = require('../assets/join.png')
 var discord = require('../assets/discord.svg')
 var facebook = require('../assets/facebook.svg')
@@ -9,17 +10,29 @@ const JoinBanner = () => {
             <div className="join mainelement">
                 <img className="banner" src={banner} alt='' />
                 <div className="join-container">
-                <div className="icon"  className="discord link">
+
+                <div  className="discord link" >
+                <a href="https://discordapp.com/invite/eXnRJaY">
+                    <span className="divLink"></span>
+                </a>
                     <h1>can't wait to hang out?</h1>
-                    <img src={discord} alt=''/><br></br>
+                    <img  className="icon" src={discord} alt=''/><br></br>
                     Join Our Discord!
                 </div>
+
                 <div className="facebook link">
+                    <a href="https://www.facebook.com/jacatucla/">
+                        <span className="divLink"></span>
+                    </a>
                     <h1>Follow Us On</h1>
                     <img className="icon" src={facebook} alt=''/><br></br>
                     For the latest updates on club events!
                 </div>
+
                 <div className="mail link">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSd1LARy-gEdloUDcXdvV6plctHIqchorBnfkr6lCXzj1knbpQ/viewform">
+                        <span className="divLink"></span>
+                    </a>
                     <h1>Join Our Mailing List</h1>
                     <img className="icon" src={mail} alt=''/><br></br>
                     For Weekly Announcements
@@ -27,5 +40,5 @@ const JoinBanner = () => {
                 </div>
             </div>
         );
-}
+    }
 export default JoinBanner;
