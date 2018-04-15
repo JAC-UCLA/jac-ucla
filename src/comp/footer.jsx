6a41ea96b1c5295f6874b465b366f82cfb31e911
mindip <oacidnim@g.ucla.edu>
2018-04-15 01:33:07 -0700

    maianh easter egg

--- src/comp/footer.jsx
+++ src/comp/footer.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import pusheen from '../assets/pusheen.png';
+import footerimg from '../assets/design18-min.png';
 
-const Footer = () => {
-        return (
-            <div className="footer-wrapper">
-                <img className="footer-img" alt='' src={require('../assets/design18-min.png')}></img>
-                <p> Email us: jacucla@gmail.com </p>
-                <p>© 2018 Japanese Animation Club at UCLA</p>
-            </div>
-        )
+class Footer extends React.Component {
+        constructor(props){
+            super(props)
+            this.state  = {
+                imgUrl: footerimg,
+            }
+            this.activatePusheen = this.activatePusheen.bind(this);
+        }
+
+        activatePusheen() {
+            this.setState({ imgUrl : pusheen})
+        }
+
+        render() {
+            return (
+                <div className="footer-wrapper">
+                    <img className="footer-img" alt='' src={this.state.imgUrl}></img>
+                    <p> Email us: jacucla@gmail.com </p>
+                    <p onClick={this.activatePusheen}>© 2018 Japanese Animation Club at UCLA</p>
+                </div>
+            )
+        }
 }
 export default Footer;
